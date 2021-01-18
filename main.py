98ed63206a5c5b1982f96fe4162bb1cd9c704bce
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-01-18 18:02:27 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 while True:
     print(input.sound_level() +input.light_level())
-    if input.light_level()> 5 + input.sound_level()>150:
+    if input.light_level()> 5 + input.sound_level()>100:
         music.wawawawaa.loop()
         light.set_pixel_color(0,light.rgb(255,255,255))
     else:
         music.stop_all_sounds()
+        light.clear()
--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 while (true) {
     console.log(input.soundLevel() + input.lightLevel())
-    if (input.lightLevel() > 5 + input.soundLevel() && 5 + input.soundLevel() > 150) {
+    if (input.lightLevel() > 5 + input.soundLevel() && 5 + input.soundLevel() > 100) {
         music.wawawawaa.loop()
         light.setPixelColor(0, light.rgb(255, 255, 255))
     } else {
         music.stopAllSounds()
+        light.clear()
     }
     
 }
