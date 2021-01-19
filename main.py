0b89188773870dd737f810e4d61191ddc932b22b
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-01-18 19:01:04 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 while True:
     if input.sound_level():
         music.play_melody("E F G F F E C5 F E B B G A F", 120)
+        light.set_all(light.rgb(255, 255, 255))
     else:
         music.stop_all_sounds()
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 while (true) {
     if (input.soundLevel()) {
         music.playMelody("E F G F F E C5 F E B B G A F", 120)
+        light.setAll(light.rgb(255, 255, 255))
     } else {
         music.stopAllSounds()
     }
