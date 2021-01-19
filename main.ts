d0b4ffde2aef4c88a9b9101b1afe9a04885b0139
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-01-19 09:59:38 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,7 +1,2 @@
-if (input.soundLevel() > 10) {
-    music.setVolume(20)
-    music.playMelody("E        Db B  A  Ab E   F#    E  E E     E       E    Db    B   A       Ab    E     F#  E E", 120)
-} else {
-    music.stopAllSounds()
-}
-
+music.setVolume(50)
+music.playMelody("E        Db B  A  Ab E   F#    E  E E     E       E    Db    B   A       Ab    E     F#  E E", 120)
