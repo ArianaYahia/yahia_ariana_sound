931cf3a7f4bc6605cffd074cbda910ece5fce7aa
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-01-19 09:51:08 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 while True:
-    if input.sound_level():
+    if input.sound_level()>5:
         music.set_volume(20)
         music.play_melody("E        Db B  A  Ab E   F#    E  E E     E       E    Db    B   A       Ab    E     F#  E E", 120)
-    else:
-        music.stop_all_sounds()
+    
--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 while (true) {
-    if (input.soundLevel()) {
+    if (input.soundLevel() > 5) {
         music.setVolume(20)
         music.playMelody("E        Db B  A  Ab E   F#    E  E E     E       E    Db    B   A       Ab    E     F#  E E", 120)
-    } else {
-        music.stopAllSounds()
     }
     
 }
